bdbeb69e5c7374a5cbd7c87d4a1e0debf731be99
pacifique-I <izabayopaci8@gmail.com>
2022-04-30 09:04:47 +0000

    fixing a bug on contact button

--- script.js
+++ script.js
@@ -15,29 +15,33 @@ contactCloseButton.addEventListener("click",()=>{
     contactPage.style.right="-100%";
 });
 navigation.forEach((g)=>{
-    g.addEventListener("click",()=>{
-        index=g.dataset['position'];
-        navigation.forEach((e)=>{
-            e.classList.remove("active")
-        })
-        g.setAttribute("class","active")
-        let location = parseInt(index)*pageOffset;
-        page.scrollTo({
-            top: 0,
-            left: location,
-            behavior: "smooth"
+    if(g.classList.contains("contact")){
+        console.log("found!");
+    }else{
+        g.addEventListener("click",()=>{
+            index=g.dataset['position'];
+            navigation.forEach((e)=>{
+                e.classList.remove("active")
+            })
+            g.setAttribute("class","active")
+            let location = parseInt(index)*pageOffset;
+            page.scrollTo({
+                top: 0,
+                left: location,
+                behavior: "smooth"
 
+            })
+            dotNavigation.forEach((r)=>{
+                let checkIndex;
+                checkIndex= r.dataset['index'];
+                r.classList.remove("activeDot")
+                r.classList.add("dot")
+                if(checkIndex == index){
+                    r.setAttribute("class","activeDot");
+                }
+            });
         })
-        dotNavigation.forEach((r)=>{
-            let checkIndex;
-            checkIndex= r.dataset['index'];
-            r.classList.remove("activeDot")
-            r.classList.add("dot")
-            if(checkIndex == index){
-                r.setAttribute("class","activeDot");
-            }
-        });
-    })
+    }
 
 });
 
